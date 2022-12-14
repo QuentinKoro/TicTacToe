644c612ddbe1b9a119464abe54d4d81526f24085
Korimis <quentinradlo.pro@gmail.com>
2022-12-14 19:47:16 +0100

    Added a few functions

--- game.java
+++ game.java
@@ -3,12 +3,61 @@ public class game{
 
 	public static void main(String args[]){
 	
+		/**
 		System.out.println("Testing");
-		//Grid newgrid = new Grid();
 		int[] row0 = new int [3];
 		int[] row1 = new int [3];
 		int[] row2 = new int [3];
+		changeRow(row0, 0, 1);
+		changeRow(row0, 1, 15);
+		changeRow(row0, 2, 3);
 		GetUserInput();
+		*/
+
+		Grid jeu = new Grid();
+
+		System.out.println("\nAvant changement\n");
+		System.out.println("\nArray 0 :\n");
+		System.out.println(jeu.row0[0]);
+		System.out.println(jeu.row0[1]);
+		System.out.println(jeu.row0[2]);
+		System.out.println("\nArray 1 :\n");
+		System.out.println(jeu.row1[0]);
+		System.out.println(jeu.row1[1]);
+		System.out.println(jeu.row1[2]);
+		System.out.println("\nArray 2 :\n");
+		System.out.println(jeu.row2[0]);
+		System.out.println(jeu.row2[1]);
+		System.out.println(jeu.row2[2]);
+
+		valueInArray(jeu, 0, 0, 0);
+		changeValueArray(jeu, 0, 0, 443);
+
+		valueInArray(jeu, 0, 0, 0);
+		valueInArray(jeu, 0, 0, 443);
+		System.out.println("\nApres changement\n");
+		System.out.println("\nArray 0 :\n");
+		System.out.println(jeu.row0[0]);
+		System.out.println(jeu.row0[1]);
+		System.out.println(jeu.row0[2]);
+		System.out.println("\nArray 1 :\n");
+		System.out.println(jeu.row1[0]);
+		System.out.println(jeu.row1[1]);
+		System.out.println(jeu.row1[2]);
+		System.out.println("\nArray 2 :\n");
+		System.out.println(jeu.row2[0]);
+		System.out.println(jeu.row2[1]);
+		System.out.println(jeu.row2[2]);
+		changeValueArray(jeu, 0, 0, 2);
+		placeValueInArray(jeu);
+
+	}
+	//Class grid which contains every list for every row
+	static class Grid{
+
+		int[] row0 = new int [3];
+		int[] row1 = new int [3];
+		int[] row2 = new int [3];
 	}
 	/**
 	 * @param None
@@ -30,13 +79,92 @@ public class game{
 			input = in.nextLine();
 		}
 
-		//Changes input, to int 
-		
-		System.out.println(input_int);
+		//Changes input(a string), to an int 	
 		return input_int;
 
 	}
+	/**
+	 * @param An array to change
+	 * @param A int index which represents the place of the array to change
+	 * @param A int, which will replace the value sitting at index place
+	 * @return The inputList with value at the index place
+	 */
+	static int[] changeRow(int[] inputArray, int index, int value){
 
+		switch(index){
 
+			case 0:
+				inputArray[index] = value;
+				break;
 
+			case 1:
+				inputArray[index] = value;
+				break;
+
+			case 2:
+				inputArray[index] = value;
+				break;
+		}
+
+		return inputArray;	
+	}	
+	static Grid changeValueArray(Grid gameGrid, int ArrayToChange, int index, int value){
+		switch(ArrayToChange){
+			case 0:
+				changeRow(gameGrid.row0, index, value);
+				break;
+
+			case 1:
+				changeRow(gameGrid.row1, index, value);
+				break;
+
+			case 2:
+				changeRow(gameGrid.row2, index, value);
+				break;
+		}
+		return gameGrid; 
+	}
+	//Checks if the value "value" is at the place of index in the array selected 
+	static boolean valueInArray(Grid gameGrid, int ArrayToChange, int index, int value){
+		boolean valueIn = false;
+		switch(ArrayToChange){
+			case 0:
+				if (gameGrid.row0[index] == value){
+					System.out.println("It is true");}
+					valueIn = true;
+				break;
+
+			case 1:
+				if (gameGrid.row1[index] == value){
+					valueIn = true;
+				}
+				break;
+
+			case 2:
+				if (gameGrid.row2[index] == value){
+					valueIn = true;
+				}
+				break;
+		}
+		return valueIn;
+	}
+
+	//Places a value in the arrays
+	static Grid placeValueInArray(Grid gameGrid){
+		//Getting value from user
+		System.out.println("\nSetting an x\n");
+		int x = GetUserInput();
+		System.out.println("\nSetting an y\n");
+		int y = GetUserInput();
+		//We check if there is already a value where the user wants to go
+		//If there is we make the user change it until it's good
+		while((valueInArray(gameGrid, y, x, 1) || (valueInArray(gameGrid, y, x, 2)))){
+			System.out.println("\nError there is already a value there, please try again somewhere else\n");		
+			System.out.println("\nSetting an x\n");
+			x = GetUserInput();
+			System.out.println("\nSetting an y\n");
+			y = GetUserInput();
+		}
+		return gameGrid;
+		}
 }
